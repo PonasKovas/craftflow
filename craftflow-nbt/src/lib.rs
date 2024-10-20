1d41ab3196f014ee83d66feb69e22ac4e982c1ff
PonasKovas <mykolas.peteraitis@gmail.com>
2024-10-20 19:55:22 +0300

    add logo to nbt crate docs

--- craftflow-nbt/src/lib.rs
+++ craftflow-nbt/src/lib.rs
@@ -1,3 +1,10 @@
+#![doc(
+	html_favicon_url = "https://github.com/PonasKovas/craftflow/blob/master/assets/icon.png?raw=true"
+)]
+#![doc(
+	html_logo_url = "https://github.com/PonasKovas/craftflow/blob/master/assets/icon.png?raw=true"
+)]
+
 pub mod arrays;
 mod error;
 pub(crate) mod ser;
