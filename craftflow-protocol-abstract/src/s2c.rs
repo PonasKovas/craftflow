8ab74b3993e3407f7372236d1ff91ac6ab42a061
PonasKovas <mykolas.peteraitis@gmail.com>
2024-10-30 18:48:06 +0200

    generalise disconnect packet

--- craftflow-protocol-abstract/src/s2c.rs
+++ craftflow-protocol-abstract/src/s2c.rs
@@ -1,5 +1,4 @@
 pub mod conf_add_resource_pack;
-pub mod conf_disconnect;
 pub mod conf_feature_flags;
 pub mod conf_finish;
 pub mod conf_keepalive;
@@ -9,8 +8,8 @@ pub mod conf_registry;
 pub mod conf_remove_resource_pack;
 pub mod conf_reset_chat;
 pub mod conf_tags;
+pub mod disconnect;
 pub mod login_compress;
-pub mod login_disconnect;
 pub mod login_encryption_begin;
 pub mod login_plugin;
 pub mod login_success;
@@ -18,15 +17,14 @@ pub mod status_info;
 pub mod status_pong;
 
 pub use conf_add_resource_pack::AbConfAddResourcePack;
-pub use conf_disconnect::AbConfDisconnect;
 pub use conf_feature_flags::AbConfFeatureFlags;
 pub use conf_finish::AbConfFinish;
 pub use conf_plugin::AbConfPlugin;
 pub use conf_remove_resource_pack::AbConfRemoveResourcePack;
 pub use conf_reset_chat::AbConfResetChat;
 pub use conf_tags::AbConfTags;
+pub use disconnect::AbDisconnect;
 pub use login_compress::AbLoginCompress;
-pub use login_disconnect::AbLoginDisconnect;
 pub use login_encryption_begin::AbLoginEncryptionBegin;
 pub use login_plugin::AbLoginPluginRequest;
 pub use login_success::AbLoginSuccess;
@@ -42,13 +40,12 @@ gen_direction_enum! {
 	pub enum AbS2C {
 		StatusInfo(AbStatusInfo),
 		StatusPong(AbStatusPong),
-		LoginDisconnect(AbLoginDisconnect),
+		Disconnect(AbDisconnect),
 		LoginEncryptionBegin(AbLoginEncryptionBegin),
 		LoginSuccess(AbLoginSuccess),
 		LoginCompress(AbLoginCompress),
 		LoginPluginRequest(AbLoginPluginRequest),
 		ConfPlugin(AbConfPlugin),
-		ConfDisconnect(AbConfDisconnect),
 		ConfFinish(AbConfFinish),
 		ConfAddResourcePack(AbConfAddResourcePack),
 		ConfRemoveResourcePack(AbConfRemoveResourcePack),
--- craftflow-protocol-abstract/src/s2c/disconnect.rs
+++ craftflow-protocol-abstract/src/s2c/disconnect.rs
@@ -2,42 +2,44 @@ use crate::{AbPacketNew, AbPacketWrite, ConstructorResult, NoConstructor, State,
 use anyhow::Result;
 use craftflow_protocol_core::{common_structures::Text, datatypes::Json};
 use craftflow_protocol_versions::{
-	s2c::{
-		login::{disconnect::v00765::DisconnectV00005, Disconnect},
-		Login,
-	},
+	s2c::{self, Configuration, Login},
 	IntoStateEnum, S2C,
 };
 use std::iter::{once, Once};
 
 /// Disconnects the client and displays the given message.
+/// Available in login, configuration and play states
 #[derive(Debug, Clone, PartialEq, Hash, Eq, PartialOrd, Ord)]
-pub struct AbLoginDisconnect {
+pub struct AbDisconnect {
 	pub message: Text,
 }
 
-impl AbPacketWrite for AbLoginDisconnect {
+impl AbPacketWrite for AbDisconnect {
 	type Direction = S2C;
 	type Iter = Once<Self::Direction>;
 
 	fn convert(self, _protocol_version: u32, state: State) -> Result<WriteResult<Self::Iter>> {
-		if state != State::Login {
-			return Ok(WriteResult::Unsupported);
-		}
-
-		// This packet is identical in all protocol versions
-		Ok(WriteResult::Success(once(
-			DisconnectV00005 {
+		let pkt = match state {
+			State::Login => s2c::login::disconnect::v00005::DisconnectV00005 {
 				reason: Json {
 					inner: self.message,
 				},
 			}
 			.into_state_enum(),
-		)))
+			State::Configuration => s2c::configuration::disconnect::v00764::DisconnectV00764 {
+				reason: Json {
+					inner: self.message,
+				},
+			}
+			.into_state_enum(),
+			_ => return Ok(WriteResult::Unsupported),
+		};
+
+		Ok(WriteResult::Success(once(pkt)))
 	}
 }
 
-impl AbPacketNew for AbLoginDisconnect {
+impl AbPacketNew for AbDisconnect {
 	type Direction = S2C;
 	type Constructor = NoConstructor<Self, S2C>;
 
@@ -45,11 +47,16 @@ impl AbPacketNew for AbLoginDisconnect {
 		packet: Self::Direction,
 	) -> Result<ConstructorResult<Self, Self::Constructor, Self::Direction>> {
 		match packet {
-			S2C::Login(Login::Disconnect(Disconnect::V00005(packet))) => {
+			S2C::Login(Login::Disconnect(s2c::login::Disconnect::V00005(packet))) => {
 				Ok(ConstructorResult::Done(Self {
 					message: packet.reason.inner,
 				}))
 			}
+			S2C::Configuration(Configuration::Disconnect(
+				s2c::configuration::Disconnect::V00764(packet),
+			)) => Ok(ConstructorResult::Done(Self {
+				message: packet.reason.inner,
+			})),
 			_ => Ok(ConstructorResult::Ignore(packet)),
 		}
 	}
