1559e8e68fd7ac73237029264a7c868d4802560d
PonasKovas <mykolas.peteraitis@gmail.com>
2025-03-13 17:53:33 +0200

    ok enough of this performance slop

--- craftflow-nbt/src/internal/swap_endian.rs
+++ craftflow-nbt/src/internal/swap_endian.rs
@@ -1,38 +1,64 @@
 #[cfg(target_endian = "big")]
+#[inline]
 pub fn swap_endian(slice: &mut [u8], element_size: usize) {}
 
 #[cfg(target_endian = "little")]
+#[inline]
 pub fn swap_endian(slice: &mut [u8], element_size: usize) {
+	if element_size == 1 {
+		return;
+	}
+
 	#[cfg(not(feature = "nightly"))]
-	for chunk in slice.chunks_mut(element_size) {
-		chunk.reverse();
+	match element_size {
+		2 => scalar::<2>(slice),
+		4 => scalar::<4>(slice),
+		8 => scalar::<8>(slice),
+		_ => unreachable!(),
 	}
 
 	#[cfg(feature = "nightly")]
-	simd(slice, element_size)
+	match element_size {
+		2 => simd::<2>(slice),
+		4 => simd::<4>(slice),
+		8 => simd::<8>(slice),
+		_ => unreachable!(),
+	}
+}
+
+#[inline]
+fn scalar<const N: usize>(slice: &mut [u8]) {
+	for chunk in slice.chunks_mut(N) {
+		chunk.reverse();
+	}
 }
 
 #[cfg(feature = "nightly")]
-fn simd(slice: &mut [u8], element_size: usize) {
-	use std::simd::Simd;
+#[inline]
+fn simd<const N: usize>(slice: &mut [u8]) {
+	use std::simd::simd_swizzle;
 
-	let shuffle = match element_size {
-		1 => return,
-		2 => Simd::from_array([1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14]),
-		4 => Simd::from_array([3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12]),
-		8 => Simd::from_array([7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8]),
-		_ => unreachable!(),
-	};
-
-	for chunk in slice.chunks_mut(16) {
-		if chunk.len() == 16 {
-			let data = Simd::<u8, 16>::from_slice(chunk);
-			data.scatter(chunk, shuffle);
+	let (start, simd, end) = slice.as_simd_mut::<16>();
+	scalar::<N>(start);
+	scalar::<N>(end);
+	for simd in simd {
+		*simd = if N == 2 {
+			simd_swizzle!(
+				*simd,
+				[1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14]
+			)
+		} else if N == 4 {
+			simd_swizzle!(
+				*simd,
+				[3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12]
+			)
+		} else if N == 8 {
+			simd_swizzle!(
+				*simd,
+				[7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8]
+			)
 		} else {
-			// fallback for leftover bytes
-			for element in chunk.chunks_mut(element_size) {
-				element.reverse();
-			}
+			unreachable!()
 		}
 	}
 }
