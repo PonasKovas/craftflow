4a33cca10db6a8a5aee0980fecfe9e8d8f306654
PonasKovas <mykolas.peteraitis@gmail.com>
2025-03-22 01:04:25 +0200

    delete old slop

--- craftflow-protocol-versions/c2s/handshaking/set_protocol/v00005/mod.rs
+++
@@ -1,9 +0,0 @@
-define_type! {
-	#[derive(ShallowClone, MakeOwned, Debug, PartialEq, Clone, Hash)]
-	pub struct SetProtocolV00005<'a> {
-		pub protocol_version: VarInt,
-		pub server_host: Cow<'a, str>,
-		pub server_port: u16,
-		pub next_state: VarInt,
-	}
-}
--- craftflow-protocol-versions/src/test_prompt_example.rs
+++
@@ -1,9 +0,0 @@
-use craftflow_nbt::DynNBT;
-use craftflow_protocol_core::datatypes::*;
-use craftflow_protocol_core::*;
-use shallowclone::{MakeOwned, ShallowClone};
-use std::borrow::Cow;
-
-// This tests whether the LLM prompt example code is actually correct for the current codebase
-
-include!("../prompt_example_code.rs");
